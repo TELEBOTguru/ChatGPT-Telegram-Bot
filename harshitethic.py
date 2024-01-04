57a3d7e3539367a6dfb6fec928be79b7a6312911
TELEBOTguru <155661039+TELEBOTguru@users.noreply.github.com>
2024-01-04 22:12:36 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6540967245:AAHLGpy95lDs-XDNhI6Do7kQ2AJG5SBRUZ0"
 else:
     tgkey = tgenv
 print(tgenv)
